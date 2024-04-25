ba0324d69f0e181e22466c7b83b26e0f2a8c38c1
Issam <149753820+elhadjissam2003@users.noreply.github.com>
2024-04-25 17:42:41 +0200

    Update minimun-array.c

--- minimun-array.c
+++ minimun-array.c
@@ -11,3 +11,29 @@ int main() {
     printf("Min=%d",min);
      return 0;
 }
+
+
+// use a function 
+//  #include <stdio.h>
+
+
+// int min_arr(int arr[]){
+//     int min=arr[0];
+//     for(int i=0;i<6;i++){
+//         printf("Checking myArray[%d]=%d\n",i,arr[i]);
+//            if(arr[i]<=min){
+//             min=arr[i];
+//             printf("New Min is=%d\n",min);
+//             }
+//     }
+//     return min;
+// }
+
+// int main() {
+//     int myArray[]={1,2,1,4,18,100};
+
+
+//     printf("Min=%d",min_arr(myArray));
+//      return 0;
+// }
+
